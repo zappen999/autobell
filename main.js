8749e6c6807e0b7c8ae8f05558f691541397bb9c
Johan Kanefur <johan.canefur@gmail.com>
2018-01-30 21:08:55 +0100

    Prepared for going live

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-// var _gaq = _gaq || [];
-// _gaq.push(['_setAccount', 'UA-113237152-1']);
-// (function() {
-//   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-//   ga.src = 'https://ssl.google-analytics.com/ga.js';
-//   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-// })();
-
-// Todo:
-// Google analytics events
-
 var PROJECT_NAME = 'Autobell';
-var DEBUG = true;
+var DEBUG = false;
 var SUBSCRIBE_BTN_SELECTOR = '#subscribe-button paper-button';
 var NOTIFY_BTN_SELECTOR = '#notification-button #button';
 
